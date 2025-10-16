bc36e1fe393c11a43d2a375f3aede0c58c1702aa
lucky295nest <lukas.olbrecht@gmail.com>
2025-10-16 20:15:49 +0200

    added coc.nvim

--- init.lua
+++ init.lua
@@ -1050,6 +1050,7 @@ require('lazy').setup({
   require 'kickstart.plugins.autopairs',
   require 'kickstart.plugins.neo-tree',
   require 'custom.plugins.noice',
+  -- require 'custom.plugins.coc', -- requires npm
   -- require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
 
   -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
---
+++ lua/custom/plugins/coc.lua
@@ -0,0 +1,21 @@
+return {
+  'neoclide/coc.nvim',
+  branch = 'release',
+  build = 'npm ci',
+  config = function()
+    -- Optional: show diagnostic signs
+    vim.fn.sign_define('CocDiagnosticSignError', { text = '✘', texthl = 'CocErrorSign' })
+    vim.fn.sign_define('CocDiagnosticSignWarning', { text = '▲', texthl = 'CocWarningSign' })
+    vim.fn.sign_define('CocDiagnosticSignInformation', { text = '', texthl = 'CocInfoSign' })
+    vim.fn.sign_define('CocDiagnosticSignHint', { text = '⚑', texthl = 'CocHintSign' })
+
+    -- Optional: keymaps for common Coc actions
+    local opts = { silent = true, noremap = true, expr = true }
+    vim.api.nvim_set_keymap('i', '<TAB>', 'coc#pum#visible() ? coc#pum#next(1) : "\\<TAB>"', opts)
+    vim.api.nvim_set_keymap('i', '<S-TAB>', 'coc#pum#visible() ? coc#pum#prev(1) : "\\<S-TAB>"', opts)
+    vim.api.nvim_set_keymap('n', 'gd', '<Plug>(coc-definition)', {})
+    vim.api.nvim_set_keymap('n', 'gy', '<Plug>(coc-type-definition)', {})
+    vim.api.nvim_set_keymap('n', 'gi', '<Plug>(coc-implementation)', {})
+    vim.api.nvim_set_keymap('n', 'gr', '<Plug>(coc-references)', {})
+  end,
+}
