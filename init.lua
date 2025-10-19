fd510ae9ba008761095c917bd93a07d282d0272a
lucky295nest <lukas.olbrecht@gmail.com>
2025-10-19 02:01:59 +0200

    kanagawa theme tweaks

--- init.lua
+++ init.lua
@@ -932,33 +932,21 @@ require('lazy').setup({
     'rebelot/kanagawa.nvim',
     priority = 1000,
     config = function()
-      -- Default options:
       require('kanagawa').setup {
-        compile = false, -- enable compiling the colorscheme
-        undercurl = true, -- enable undercurls
+        compile = false,
+        undercurl = true,
         commentStyle = { italic = true },
         functionStyle = {},
         keywordStyle = { italic = true },
         statementStyle = { bold = true },
         typeStyle = {},
-        transparent = true, -- do not set background color
-        dimInactive = false, -- dim inactive window `:h hl-NormalNC`
-        terminalColors = true, -- define vim.g.terminal_color_{0,17}
-        colors = { -- add/modify theme and palette colors
-          palette = {},
-          theme = { wave = {}, lotus = {}, dragon = {}, all = {} },
-        },
-        overrides = function(colors) -- add/modify highlights
-          return {}
-        end,
-        theme = 'wave', -- Load "wave" theme
-        background = { -- map the value of 'background' option to a theme
-          dark = 'dragon', -- try "dragon" !
-          light = 'lotus',
-        },
+        transparent = false,
+        dimInactive = false,
+        terminalColors = true,
+        colors = { palette = {}, theme = { wave = {}, lotus = {}, dragon = {}, all = {} } },
       }
 
-      -- setup must be called before loading
+      -- Your preferred colorscheme, applied last
       vim.cmd.colorscheme 'solarized-osaka'
     end,
   },
